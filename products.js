cd47e57cf0ae6fe16f09e278a953c74c9017528f
billel01 <106335815+billel01@users.noreply.github.com>
2022-05-27 19:53:22 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,18 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.png',
+		image3: 'img/products/3.png',
+		image4: 'img/products/4.png',
+		image5: 'img/products/5.png',
+		title: '  مبرد محمول 48 لتر 4 في 1 سهلة التنقل ومثالي لجميع رحلاتك التخييمة Glacière Portative 4 en 1 de 48 L est facile à déplacer et idéale pour toutes vos sorties en camping ',
+		price1: 8900,
+		price: 7900,
+		description: ' مع حلول موسم الاصطياف والاستجمام أحصل الان على برادة محمولة بسعة 48 لتر. مثالية لإبقاء مأكولاتك طازجة بل و تبريد الفواكه و المشروبات و مزودة بعجلات للتنقل بكل أريحية. و تتحول لتصبح طاولة سفر محمولة مع كرسيين مدمجين لتحظى بلمة العائلة و الأحباب ما يجعلها من القطع الأساسية أثناء رحلاتك التخييمية والتنزه اغتنم الفرصة و قدم طلبك قبل نفاذ الكمية.'   ,
+		phone: 0770439221,
+	},
+
+	
+];  
